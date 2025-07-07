c37b940ab2b0b945300dd589dae78d35bd9c34b8
ce482 <ce482@uchicago.edu>
2025-07-07 18:07:23 -0400

    issues

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -1,3 +1,4 @@
+
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
